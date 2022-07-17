1a03ceda04a97c634329114e70784eb099c34681
vivcyber <mspinksun@gmail.com>
2022-07-17 22:30:33 +0800

    1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
   return (
     <div className='navbar'>
         <div className='leftSide'> 
-        <img src={Logo} />
+        <img src={Logo} alt="logo" />
         </div>
         <div className='rightSide'>
             <Link to="/home">Home</Link>
